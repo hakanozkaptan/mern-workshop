438acc1f0124380c57de437293e94bd3d019201b
Hakan Ozkaptan <ozkaptanhakan@gmail.com>
2020-01-22 00:04:35 +0300

    branch test

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = () => (
   <div className='card bg-dark text-center text-light rounded-0 mt-5'>
-    <h1 className='display-4'>
+    <h1 className='display-6'>
       <i className='fas fa-clipboard-list mr3'></i>
       <span className='text-light ml-3'>MERN</span> Todo List
     </h1>
