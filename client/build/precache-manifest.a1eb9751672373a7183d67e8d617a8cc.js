412fb56570aad47cc6b3e267e77c3bdb5f42fc64
Hakan Ozkaptan <ozkaptanhakan@gmail.com>
2020-09-06 02:21:22 +0300

    heroku deploy

---
+++ client/build/precache-manifest.a1eb9751672373a7183d67e8d617a8cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215651e1fcf64801c7752a7db7f49f02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c443ce1da94ed282b81d",
+    "url": "/static/js/2.a81d85a7.chunk.js"
+  },
+  {
+    "revision": "40aad3a8e714aaa8b071e4e33d516e64",
+    "url": "/static/js/2.a81d85a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9a91403916a09f516e0",
+    "url": "/static/js/main.de0dc6ae.chunk.js"
+  },
+  {
+    "revision": "65326020070271871721",
+    "url": "/static/js/runtime-main.2df13cd9.js"
+  }
+]);
